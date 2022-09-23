15e67ea7a2efb7117fbcd9d4c59b7888795753cc
tlatimer <tom@tomlat.com>
2022-09-22 21:58:38 -0500

    new stub

---
+++ memoize.py
@@ -0,0 +1,15 @@
+class Memoize:
+    def __init__(self, fn):
+        self.fn = fn
+        self.memo = {}
+
+    def __call__(self, *args):
+        if args not in self.memo:
+            self.memo[args] = self.fn(*args)
+        return self.memo[args]
+
+
+# Example
+@Memoize
+def function(args):
+    pass
